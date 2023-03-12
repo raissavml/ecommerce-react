69b5e331afb09811e42ecdb9eb23719b8edf7873
enzinho <enzo.maxpayne@gmail.com>
2023-03-12 13:17:26 -0300

    corrigindo erro no login do usuario

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
+
+// == Images ==
+import olho1 from '../../imagens/icones/olho1.png'
+
 export default function Login() {
   const [users, setUsers] = React.useState([]);
   const [currentUser, setCurrentUser] = React.useState("");
@@ -20,14 +25,15 @@ export default function Login() {
         <section>
           <input type="text" placeholder="Login" />
           <input type="password" className="senha" placeholder="Senha" />
-          <img src="../Imagens/icones/olho1.png" className="btn" alt="" />
+          <img src={olho1} className="btn" alt="" />
           <button onClick={handleLogin} className="button">
             Entrar
             {users.map((user) => (
-              value={user.password})
-              (user.id) => setCurrentUser(user.id)
-            )
-            }
+              <>
+                value={user.password})
+                (user.id) => setCurrentUser(user.id)
+              </>
+            ))}
           </button>
           <Link to="/">Esqueceu sua senha?</Link>
         </section>
